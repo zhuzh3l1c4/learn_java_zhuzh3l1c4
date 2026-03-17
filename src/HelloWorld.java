d2917b7e3d0e56653db7477322577d28ae225cb2
zhuzh3l1c4 <127420433+zhuzh3l1c4@users.noreply.github.com>
2026-03-17 21:46:11 +0200

    Update HelloWorld.java
    years multiplicator changed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         int myAge = 17;
         int dogsAge = 6;
-        int multiplicator = 7;
+        int multiplicator = 8;
         int answer = dogsAge * multiplicator;
         System.out.println("My dog's age in man years is " + answer);
     }
