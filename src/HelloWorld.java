0cd9a8694c076ed2d8159d390c8745eb97d1cd3e
kiploks <zhuzh3l1c4@gmail.com>
2026-03-17 21:41:34 +0200

    dogs age calculator added

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myAge = 17;
+        int dogsAge = 6;
+        int multiplicator = 7;
+        int answer = dogsAge * multiplicator;
+        System.out.println("My dog's age in human years is " + answer);
+    }
+}
